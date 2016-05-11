4bc35a2ad2e9ba1612c486ba294ac4f11a641506
kazeidesign <contact@kazeidesign.fr>
2016-05-11 05:14:21 +0000

    Include AngularJS
    Include AngularJS files = .html in /views + .js components and .js views in /assets/js.
    Keep .ejs files

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'ngSails',
+  'ngMaterial',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
